c9590eaf5274e46fd060e71b43a95b57a3e8abc5
yeliu <yeliu@deepmirror.com>
2023-09-18 20:14:58 +0800

    opencv video

--- make_video.py
+++ make_video.py
@@ -18,50 +18,65 @@ end_cnt = 377
 fps = 25
 resize_factor = 1
 
-# print("make rgb video cv")
-# opencv_video = None
-# for i in range(start_cnt, end_cnt):
-#     if i%30 == 0:
-#         print("process", i, "/", end_cnt)
-#     rgb = cv2.imread(opt.output_path + "/tmp/" + str(i) + "rgb.jpg")
-#     rgb_size = (int(rgb.shape[1] * resize_factor), int(rgb.shape[0] * resize_factor))
-#     rgb = cv2.resize(rgb, rgb_size)
-#     if opencv_video is None:
-#         fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-#         opencv_video = cv2.VideoWriter(opt.output_path + '/cv_mpi_rgb.mp4', fourcc, 25, rgb_size)
-#     opencv_video.write(rgb)
-# opencv_video.release()
-# opencv_video = None
-
-
-print("make rgb video")
-rgb_frames = []
+# opencv video make better quality (even larger size)
+print("make rgb video cv")
+opencv_video = None
 for i in range(start_cnt, end_cnt):
     if i%30 == 0:
         print("process", i, "/", end_cnt)
     rgb = cv2.imread(opt.output_path + "/tmp/" + str(i) + "rgb.jpg")
-    rgb_frames.append(cv2.resize(rgb, (int(rgb.shape[1] * resize_factor), int(rgb.shape[0] * resize_factor))))
-    gc.collect()
+    rgb_size = (int(rgb.shape[1] * resize_factor), int(rgb.shape[0] * resize_factor))
+    rgb = cv2.resize(rgb, rgb_size)
+    if opencv_video is None:
+        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+        opencv_video = cv2.VideoWriter(opt.output_path + '/cv_mpi_rgb.mp4', fourcc, 25, rgb_size)
+    opencv_video.write(rgb)
+opencv_video.release()
 
-video_path = opt.output_path + '/mpi_rgb.mp4'
-rgb_clip = ImageSequenceClip(rgb_frames, fps=fps)
-# https://zulko.github.io/moviepy/ref/VideoClip/VideoClip.html#moviepy.video.VideoClip.VideoClip.write_videofile
-rgb_clip.write_videofile(video_path, codec='mpeg4', bitrate='8000k')
-rgb_frames.clear()
-gc.collect()
-
-print("make alpha video")
-alpha_frames = []
+print("make alpha video cv")
+opencv_video = None
 for i in range(start_cnt, end_cnt):
     if i%30 == 0:
         print("process", i, "/", end_cnt)
-    alpha = cv2.imread(opt.output_path + "/tmp/" + str(i) + "alpha.jpg")
-    alpha_frames.append(cv2.resize(alpha, (int(alpha.shape[1] * resize_factor), int(alpha.shape[0] * resize_factor))))
+    rgb = cv2.imread(opt.output_path + "/tmp/" + str(i) + "alpha.jpg")
+    rgb_size = (int(rgb.shape[1] * resize_factor), int(rgb.shape[0] * resize_factor))
+    rgb = cv2.resize(rgb, rgb_size)
+    if opencv_video is None:
+        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+        opencv_video = cv2.VideoWriter(opt.output_path + '/cv_mpi_alpha.mp4', fourcc, 25, rgb_size)
+    opencv_video.write(rgb)
+opencv_video.release()
 
-# MP4 not supported alpha channel so make a new file for alpha
-alpha_path = opt.output_path + '/mpi_alpha.mp4'
-rgb_clip_alpha = ImageSequenceClip(alpha_frames, fps=fps)
-rgb_clip_alpha.write_videofile(alpha_path, codec='mpeg4', bitrate='2000k')
-alpha_frames.clear()
+
+if False:
+    print("make rgb video")
+    rgb_frames = []
+    for i in range(start_cnt, end_cnt):
+        if i%30 == 0:
+            print("process", i, "/", end_cnt)
+        rgb = cv2.imread(opt.output_path + "/tmp/" + str(i) + "rgb.jpg")
+        rgb_frames.append(cv2.resize(rgb, (int(rgb.shape[1] * resize_factor), int(rgb.shape[0] * resize_factor))))
+        gc.collect()
+
+    video_path = opt.output_path + '/mpi_rgb.mp4'
+    rgb_clip = ImageSequenceClip(rgb_frames, fps=fps)
+    # https://zulko.github.io/moviepy/ref/VideoClip/VideoClip.html#moviepy.video.VideoClip.VideoClip.write_videofile
+    rgb_clip.write_videofile(video_path, codec='mpeg4', bitrate='8000k')
+    rgb_frames.clear()
+    gc.collect()
+
+    print("make alpha video")
+    alpha_frames = []
+    for i in range(start_cnt, end_cnt):
+        if i%30 == 0:
+            print("process", i, "/", end_cnt)
+        alpha = cv2.imread(opt.output_path + "/tmp/" + str(i) + "alpha.jpg")
+        alpha_frames.append(cv2.resize(alpha, (int(alpha.shape[1] * resize_factor), int(alpha.shape[0] * resize_factor))))
+
+    # MP4 not supported alpha channel so make a new file for alpha
+    alpha_path = opt.output_path + '/mpi_alpha.mp4'
+    rgb_clip_alpha = ImageSequenceClip(alpha_frames, fps=fps)
+    rgb_clip_alpha.write_videofile(alpha_path, codec='mpeg4', bitrate='2000k')
+    alpha_frames.clear()
 
 print("Done")
