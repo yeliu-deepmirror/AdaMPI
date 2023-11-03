8b512ba653c019537e10682a4106b093d27436f5
yeliu <yeliu@deepmirror.com>
2023-11-03 19:52:33 +0800

    update

--- gen_vr_180.py
+++ gen_vr_180.py
@@ -9,23 +9,39 @@ import glob
 import os
 import numpy as np
 import gc
+import moviepy
 from moviepy.editor import ImageSequenceClip
 import tensorflow as tf
 from utils.render import mpi
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--video_path', type=str, default="")
 parser.add_argument('--output_path', type=str, default="debug/video_mpi")
-parser.add_argument('--ckpt_path', type=str, default="weight/adampi_32p.pth")
+parser.add_argument('--output_name', type=str, default="VR_LR")
 parser.add_argument('--start_cnt', type=int, default=1)
 parser.add_argument('--end_cnt', type=int, default=0)
 parser.add_argument('--interval', type=int, default=1)
-parser.add_argument('--fps', type=int, default=30)
-parser.add_argument('--len_distance', type=float, default=0.02)
+parser.add_argument('--len_distance', type=float, default=0.01)
 opt, _ = parser.parse_known_args()
 
 # python gen_vr_180.py --output_path /mnt/gz01/experiment/liuye/mpi/video_mpi_mom_not_home --start_cnt=1 --end_cnt=300
+depths_raw = mpi.make_depths(1.0, 100.0, 8 * 4).numpy()
+print("depths_raw mpi:", depths_raw)
 
-def render(alphas, rgbs, cols = 8, rows = 4):
+fps = 30
+
+audio_clip = None
+if opt.video_path:
+    video = moviepy.editor.VideoFileClip(opt.video_path)
+    fps = video.fps
+    print("video fps :", fps)
+    audio_clip = video.audio
+    if audio_clip is not None:
+        print("load audio from :", opt.video_path)
+        audio_clip.write_audiofile(opt.output_path + "/audio.mp3")
+
+
+def render(alphas, rgbs, depths, cols = 8, rows = 4):
     # make alpha and rgb to layers : [..., L, H, W, C+1] MPI layers back to front, alpha in last channel.
     ele_width = int(alphas.shape[1] / cols)
     ele_height = int(alphas.shape[0] / rows)
@@ -44,7 +60,7 @@ def render(alphas, rgbs, cols = 8, rows = 4):
     layers = tf.convert_to_tensor(layers_np, dtype=tf.float32)
 
     # The reference camera position can just be the identity
-    depths = mpi.make_depths(1.0, 100.0, cols * rows).numpy()
+    # depths = mpi.make_depths(1.0, 100.0, cols * rows).numpy()
     reference_pose = tf.constant(
         [[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0]])
 
@@ -68,7 +84,7 @@ def render(alphas, rgbs, cols = 8, rows = 4):
         rended_images.append(image_cv)
 
     # make LR image
-    image_LR = np.concatenate([rended_images[0], rended_images[1]], axis=1)
+    image_LR = np.concatenate([rended_images[1], rended_images[0]], axis=1)
     return image_LR
 
 end_index = opt.end_cnt
@@ -78,24 +94,44 @@ if end_index < opt.start_cnt:
     end_index = int(len(images) / 2)
 
 # opencv video make better quality (even larger size)
+output_video_path = opt.output_path + "/" + opt.output_name + '.mp4'
 print("make VR LR 180 video cv.", opt.start_cnt, opt.interval, end_index)
+print("save to", output_video_path)
 opencv_video_vr = None
 for i in range(opt.start_cnt, end_index, opt.interval):
     if i%30 == 0:
-        print("process", i, "/", opt.end_cnt)
+        print("process", i, "/", end_index)
     alphas = cv2.imread(opt.output_path + "/tmp/" + str(i) + "alpha.jpg")
     rgbs = cv2.imread(opt.output_path + "/tmp/" + str(i) + "rgb.jpg")
 
+    # read depth if available
+    depths = depths_raw
+    depths_file = opt.output_path + "/tmp/" + str(i) + "depths.npy"
+    if os.path.exists(depths_file):
+        with open(depths_file, 'rb') as f:
+            depths = np.load(f).squeeze()
+    else:
+        print("Warning: depth not found for", i)
+
     # render LR images
-    image_LR = render(alphas, rgbs)
+    image_LR = render(alphas, rgbs, depths)
     image_LR_size = (int(image_LR.shape[1]), int(image_LR.shape[0]))
 
     # write the image
     if opencv_video_vr is None:
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        opencv_video_vr = cv2.VideoWriter(opt.output_path + '/VR_LR.mp4', fourcc, opt.fps, image_LR_size)
+        opencv_video_vr = cv2.VideoWriter(output_video_path, fourcc, fps, image_LR_size)
     opencv_video_vr.write(image_LR)
-opencv_video_vr.release()
+
+if opencv_video_vr is not None:
+    opencv_video_vr.release()
+
+
+if audio_clip is not None:
+    video_clip = moviepy.editor.VideoFileClip(output_video_path)
+    audio_clip = audio_clip.subclip(0, video_clip.end)
+    final_clip = video_clip.set_audio(audio_clip)
+    final_clip.write_videofile(opt.output_path + "/" + opt.output_name + '_with_audio.mp4')
 
 
 print("Done")
